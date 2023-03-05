c8e4bd411116d5bb45949f825741a799adf93381
Novacovici Sonia <sonianovacovici@gmail.com>
2023-03-05 12:39:47 +0200

    Initial commit

---
+++ backend/packingplans/src/main/java/com/cb/packingplans/PackingplansApplication.java
@@ -0,0 +1,13 @@
+package com.cb.packingplans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackingplansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackingplansApplication.class, args);
+	}
+
+}
---
+++ backend/packingplans/src/test/java/com/cb/packingplans/PackingplansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cb.packingplans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackingplansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
