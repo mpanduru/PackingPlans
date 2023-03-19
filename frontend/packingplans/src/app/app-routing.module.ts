ae90ae1bb3bd75b2e197327b43ecd501001caf53
mpanduru <mariuspanduru588@gmail.com>
2023-03-19 23:51:53 +0200

    create login interface

--- frontend/packingplans/src/app/app-routing.module.ts
+++ frontend/packingplans/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {RegisterButtonComponent, RegisterComponent} from "./components/register/register.component";
+import {LoginButtonComponent, LoginComponent} from "./components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegisterButtonComponent, children: [{path: 'dialog', component: RegisterComponent}]},
+  {path: 'login', component: LoginButtonComponent, children: [{path: 'dialog', component: LoginComponent}]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/packingplans/src/app/components/navbar/navbar.component.ts
+++ frontend/packingplans/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {LoginComponent} from "../login/login.component";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private dialog: MatDialog) {
+  }
 
+  openLoginDialog(): void {
+    this.dialog.open(LoginComponent, {
+      width: '50%',
+      height: '55%'
+    });
+  }
 }
