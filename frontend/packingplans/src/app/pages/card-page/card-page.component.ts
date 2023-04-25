919fe96d363d3b683786f7146d2158e95cf4bef2
mpanduru <mariuspanduru588@gmail.com>
2023-04-25 17:25:23 +0300

    wip2

--- frontend/packingplans/src/app/pages/card-page/card-page.component.ts
+++ frontend/packingplans/src/app/pages/card-page/card-page.component.ts
@@ -1,10 +1,19 @@
-import {Component} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card-page',
   templateUrl: './card-page.component.html',
   styleUrls: ['./card-page.component.css']
 })
-export class CardPageComponent {
-  selected: Date | undefined;
+export class CardPageComponent implements OnInit {
+  @Input() location: any | undefined;
+
+  ngOnInit() {
+    this.location = {
+      "name": "Paris",
+      "imageUrl": "../../../assets/images/paris_image.jpg",
+      "tags": ["fun", "romantic"],
+      "description": "Paris is a beautiful place in France. You will love it there"
+    }
+  }
 }
