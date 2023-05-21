62f04054ca958d9de88d0ccb2db3d94f7478d9fa
Novacovici Sonia <99807919+Sonianv@users.noreply.github.com>
2023-05-21 12:37:44 +0300

    Add styling for location page (#20)

--- backend/packingplans/src/main/java/com/cb/packingplans/PackingplansApplication.java
+++ backend/packingplans/src/main/java/com/cb/packingplans/PackingplansApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PackingplansApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PackingplansApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PackingplansApplication.class, args);
+    }
 }
